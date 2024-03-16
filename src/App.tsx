c6e539b0406580c40b123a5b4ffcdcd14a8a7674
SKornya <kornyakovS@gmail.com>
2024-03-16 23:32:23 +0300

    test header, login form, install gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
-import { MoonOutlined, SunOutlined } from '@ant-design/icons';
-import { Card, ConfigProvider, Switch, theme } from 'antd';
+import {
+  LockOutlined,
+  MoonOutlined,
+  SunOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import {
+  Button,
+  ConfigProvider,
+  Flex,
+  Form,
+  Image,
+  Input,
+  Layout,
+  Switch,
+  theme,
+} from 'antd';
+import { Content, Header } from 'antd/es/layout/layout';
 import { useEffect, useState } from 'react';
+
+import logoLight from '../public/logo_light.svg';
+import logoDark from '../public/logo_dark.svg';
+import styles from './App.module.less';
+
+interface FieldsTypes {
+  username?: string;
+  password?: string;
+}
 
 function App() {
   const [isDarkTheme, setIsDarkTheme] = useState<boolean>(
@@ -20,21 +45,84 @@ function App() {
         algorithm: isDarkTheme ? theme.darkAlgorithm : theme.defaultAlgorithm,
       }}
     >
-      <Switch
-        checkedChildren={<SunOutlined />}
-        unCheckedChildren={<MoonOutlined />}
-        onChange={() => {
-          const newMode = !isDarkTheme;
-          setIsDarkTheme(newMode);
-          document.documentElement.setAttribute(
-            'data-theme',
-            newMode ? 'dark' : 'light'
-          );
-          console.log(isDarkTheme);
-        }}
-      />
+      <Layout>
+        <Header className={styles.header}>
+          <Flex justify="space-between" align="center">
+            <Image
+              style={{ borderRadius: '10px' }}
+              width={'50px'}
+              src={isDarkTheme ? logoDark : logoLight}
+              preview={false}
+            />
 
-      <Card title="TITLE" />
+            <Switch
+              checkedChildren={<SunOutlined />}
+              unCheckedChildren={<MoonOutlined />}
+              onChange={() => {
+                const newMode = !isDarkTheme;
+                setIsDarkTheme(newMode);
+                document.documentElement.setAttribute(
+                  'data-theme',
+                  newMode ? 'dark' : 'light'
+                );
+              }}
+            />
+          </Flex>
+        </Header>
+        <Content className={styles.content}>
+          <Flex align="center" justify="center">
+            <Form<FieldsTypes>
+              name="normal_login"
+              className={styles.form}
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={() => {}}
+            >
+              <Form.Item
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your Username!',
+                  },
+                ]}
+              >
+                <Input
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  placeholder="Username"
+                />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your Password!',
+                  },
+                ]}
+              >
+                <Input
+                  prefix={<LockOutlined className="site-form-item-icon" />}
+                  type="password"
+                  placeholder="Password"
+                />
+              </Form.Item>
+
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className={styles.login_button}
+                  ghost
+                >
+                  Log in
+                </Button>
+              </Form.Item>
+            </Form>
+          </Flex>
+        </Content>
+      </Layout>
     </ConfigProvider>
   );
 }
